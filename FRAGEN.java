1fd1c6f458eeaba5c8c7dbfb5de64ec9c3a92664
lindaherzog <linda.herzog2005@icloud.com>
2021-05-06 16:38:50 +0200

    Erzeugung der Klassen Fragen und Spiel.Kurze beschreibung, was diese Klassen bewirken sollen

---
+++ FRAGEN.java
@@ -0,0 +1,33 @@
+
+/**
+ * in der Klasse Fragen werden die Fragen für das Quiz fetsgelegt.
+ * 
+ * @author Linda & Lara 
+ * @version 05.06.21
+ */
+public class FRAGEN
+{
+    // Instanzvariablen - ersetzen Sie das folgende Beispiel mit Ihren Variablen
+    private int x;
+
+    /**
+     * Konstruktor für Objekte der Klasse FRAGEN
+     */
+    public FRAGEN()
+    {
+        // Instanzvariable initialisieren
+        x = 0;
+    }
+
+    /**
+     * Ein Beispiel einer Methode - ersetzen Sie diesen Kommentar mit Ihrem eigenen
+     * 
+     * @param  y    ein Beispielparameter für eine Methode
+     * @return        die Summe aus x und y
+     */
+    public int beispielMethode(int y)
+    {
+        // tragen Sie hier den Code ein
+        return x + y;
+    }
+}
---
+++ QUIZ.java
@@ -0,0 +1,33 @@
+
+/**
+ * Die Klasse Quiz steuert die Klasse Spiel.
+ * 
+ * @author  Lara & Linda
+ * @version 05.06.21
+ */
+public class QUIZ
+{
+    // Instanzvariablen - ersetzen Sie das folgende Beispiel mit Ihren Variablen
+    private int x;
+
+    /**
+     * Konstruktor für Objekte der Klasse QUIZ
+     */
+    public QUIZ()
+    {
+        // Instanzvariable initialisieren
+        x = 0;
+    }
+
+    /**
+     * Ein Beispiel einer Methode - ersetzen Sie diesen Kommentar mit Ihrem eigenen
+     * 
+     * @param  y    ein Beispielparameter für eine Methode
+     * @return        die Summe aus x und y
+     */
+    public int beispielMethode(int y)
+    {
+        // tragen Sie hier den Code ein
+        return x + y;
+    }
+}
