ba0a44c7d5232a3f8d88748d58fe8551bae94693
LaraMintzlaff <Lara.Mintzlaff@gmx.de>
2021-05-06 16:15:13 +0200

    Version1 von Lara und Linda ( -Klasse Quiz Erstellt - -Klasse Fragen Erstellt -erste Methoden angelegt)

---
+++ FRAGEN.java
@@ -0,0 +1,65 @@
+
+/**
+ * Beschreiben Sie hier die Klasse FRAGEN.
+ * 
+ * @author (LINDA UND LARA) 
+ * @version (06.05)
+ */
+public class FRAGEN
+{
+    // Instanzvariablen - ersetzen Sie das folgende Beispiel mit Ihren Variablen
+    String fragen;
+    private int x;
+
+    /**
+     * Konstruktor für Objekte der Klasse FRAGEN
+     */
+     FRAGEN()
+    {
+        x = 0;
+        
+        
+    }
+    
+    void FrageSetzen()
+    {
+      
+        x=0;
+    }
+        
+   
+    
+    /**
+     * Methoden für die einzelnen Fragen
+     */
+    void Frage1 ()
+    {
+        // tragen Sie hier den Code ein
+        fragen = "Wie viele Zähne kann ein Hai im laufe seines Lebens haben";
+    }
+    
+    void Frage2 ()
+    {
+        // tragen Sie hier den Code ein
+        fragen = "Welches der folgenden Nagetiere hält keinen Winterschlaf";
+    }
+    
+    void Frage3 ()
+    {
+        // tragen Sie hier den Code ein
+        fragen = "Wie nennt man ein mänliches Schaf";
+    }
+    
+    void Frage4 ()
+    {
+        // tragen Sie hier den Code ein
+        fragen = "Welcher dieser Affen zählt nicht zu den Menschenaffen";
+    }
+    
+    void Frage5 ()
+    {
+        // tragen Sie hier den Code ein
+        fragen = "Wer oder was ist eine Mimose";
+    }
+  
+}
---
+++ QUIZ.java
@@ -0,0 +1,34 @@
+
+/**
+ * Beschreiben Sie hier die Klasse QUIZFRAGEN.
+ * 
+ * @author (LINDA UND LARA) 
+ * @version (eine Versionsnummer oder ein Datum)
+ */
+public class QUIZ
+{
+    // Instanzvariablen - ersetzen Sie das folgende Beispiel mit Ihren Variablen
+    
+    private int x;
+    QUIZ quizfragen;
+    /**
+     * Konstruktor für Objekte der Klasse QUIZFRAGEN
+     */
+    QUIZ()
+    {
+        // Instanzvariable initialisieren
+        x = 0;
+    }
+
+    /**
+     * Ein Beispiel einer Methode - ersetzen Sie diesen Kommentar mit Ihrem eigenen
+     * 
+     * @param  y    ein Beispielparameter für eine Methode
+     * @return        die Summe aus x und y
+     */
+    void Frage1(int y)
+    {
+        // tragen Sie hier den Code ein
+        x=0;
+    }
+}
