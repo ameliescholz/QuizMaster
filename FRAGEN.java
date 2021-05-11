ae0960495652476fc78aef6d01d0833ba33cee72
lindaherzog <linda.herzog2005@icloud.com>
2021-05-11 09:51:16 +0200

    Fragen & Antworten& Antwortmöglichkeiten in der Klasse FRAGEN definiert by Lara+Linda

--- FRAGEN.java
+++ FRAGEN.java
@@ -8,7 +8,22 @@
 public class FRAGEN
 {
     // Instanzvariablen - ersetzen Sie das folgende Beispiel mit Ihren Variablen
-    private int x;
+    
+    String Frage1;
+    String Frage2;
+    String Frage3;
+    String Frage4;
+    String Frage5;
+    String Antwort1;
+    String Antwort2;
+    String Antwort3;
+    String Antwort4;
+    String Antwort5;
+    String RichtigeAnwort1;
+    String RichtigeAnwort2;
+    String RichtigeAnwort3;
+    String RichtigeAnwort4;
+    String RichtigeAnwort5;
 
     /**
      * Konstruktor für Objekte der Klasse FRAGEN
@@ -16,18 +31,25 @@ public class FRAGEN
     public FRAGEN()
     {
         // Instanzvariable initialisieren
-        x = 0;
+    Frage1 = "Wie viele Zaehne kann ein Hai im Laufe seines Lebens haben?";
+    Frage2 = "Welche der folgenden Nagetiere haelt keinen Winterschlaf?";
+    Frage3 = "Wie nennt man ein maennliches Schaf?";
+    Frage4 = "Welcher dieser Affen zaehlt nicht zu den Menschenaffen?";
+    Frage5 = "Wer oder was ist eine Mimiose?";
+    Antwort1 = "(A) Bis zu 20 000 Zaehne   (B) Bis zu 10 000 Zaehne  (C) bis zu 2000 Zaehne"  ;
+    Antwort2 = "(A) Murmeltier  (B) Siebenschlaefer  (C) Lemming ";
+    Antwort3 = "(A) Lamm   (B) Hammel   (C) Widder ";
+    Antwort4 = "(A) Gorilla   (B) Gibbon   (C) Oran-utan";
+    Antwort5 = "(A) eine Hautkrankheit   (B)  eine tropische Pflanze  (C) ein biochemischer Vorgang ";
+    RichtigeAnwort1 = "(A)";
+    RichtigeAnwort2  = "(C)";
+    RichtigeAnwort3  = "(C)";
+    RichtigeAnwort4  = "(B)";
+    RichtigeAnwort5  = "(B)";
+
+    
+        
     }
 
-    /**
-     * Ein Beispiel einer Methode - ersetzen Sie diesen Kommentar mit Ihrem eigenen
-     * 
-     * @param  y    ein Beispielparameter für eine Methode
-     * @return        die Summe aus x und y
-     */
-    public int beispielMethode(int y)
-    {
-        // tragen Sie hier den Code ein
-        return x + y;
-    }
+    
 }
