96ce57b31435bf9fea4a69bdd33e558c4dcb871b
LaraMintzlaff <Lara.Mintzlaff@gmx.de>
2021-05-18 09:42:23 +0200

    In Klasse ZEICHENFLAECHE Fenster für das spätere Quiz erstellt  . Linda und Lara

---
+++ ZEICHENFLAECHE.java
@@ -0,0 +1,64 @@
+import java.awt.*;
+import javax. swing. *;
+import java.awt.event.*;
+/**
+ * Beschreiben Sie hier die Klasse ZEICHENFLÄCHE.
+ * 
+ * @author (Lara und Linda) 
+ * @version (eine Versionsnummer oder ein Datum)
+ */
+public class ZEICHENFLAECHE
+{
+    // Instanzvariablen - ersetzen Sie das folgende Beispiel mit Ihren Variablen
+    private static final int rasterGroesse = 40;
+    private static final int hoeheFenster = rasterGroesse * 20;
+    private static final int breiteFenster = rasterGroesse * 26;
+    private static ZEICHENFLAECHE o = null;
+    private JFrame fenster = null;
+
+    /**
+     * Konstruktor für Objekte der Klasse ZEICHENFLAECHE
+     */
+    public ZEICHENFLAECHE()
+    {
+        // Instanzvariable initialisieren
+        fenster = new JFrame ("Zeichenfläche");
+        fenster. setResizable (false);
+        fenster. setVisible (true);
+        fenster. setLayout (null);
+        fenster. getContentPane (). setBackground (new Color (240, 240, 240));
+        Insets i = fenster.getInsets();
+        fenster. setSize (breiteFenster, hoeheFenster + i.top);    
+    }
+
+    /**
+     * Ein Beispiel einer Methode - ersetzen Sie diesen Kommentar mit Ihrem eigenen
+     * 
+     * @param  y    ein Beispielparameter für eine Methode
+     * @return        die Summe aus x und y
+     */
+    static JFrame FensterGeben ()
+    {
+        if (o == null)
+        {
+            o = new ZEICHENFLAECHE ();
+        }
+        return o. fenster;
+    }
+    
+    static int RasterGroesseGeben ()
+    {
+        return rasterGroesse;
+    }
+    
+    static int FensterBreiteGeben ()
+    {
+        return breiteFenster;
+    }
+    
+    static int FensterHoeheGeben ()
+    {
+        return hoeheFenster;
+    }
+    
+}
